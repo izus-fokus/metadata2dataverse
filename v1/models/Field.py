7f2c4f5ea97a904ce1d55ee389b1c9ef1d113a8d
Anne Kreuter <anne.kreuter@outlook.de>
2020-06-24 22:07:06 +0200

    Global Variables, dynamisches TSV indexing, __repr__()

--- v1/models/Field.py
+++ v1/models/Field.py
@@ -1,16 +1,17 @@
 class Field(object):
     ''' object after parsing a tsv file '''
 
-    controlled_vocabulary = []
+    
     def __init__(self, multiple, type_class, parent, metadata_block):
         ''' Constructor '''
+        self.controlled_vocabulary = []
         self.multiple = multiple
         self.type_class = type_class
         self.parent = parent
         self.metadata_block = metadata_block
         
     def __repr__(self):
-        return "{multiple: " + str(self.multiple) + ", type class: " + self.type_class + ", parent: " + str(self.parent) + ", metadata block: " + self.metadata_block + "}"
+        return "{multiple: " + str(self.multiple) + ", type class: " + self.type_class + ", parent: " + str(self.parent) + ", metadata block: " + self.metadata_block + ", controlled Vocabulary: " + str(self.controlled_vocabulary) +"}"
         
     def set_controlled_vocabulary(self, controlled_vocabulary):
         self.controlled_vocabulary.append(controlled_vocabulary)
