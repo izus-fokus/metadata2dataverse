aec8b674c0103b03349667033177c3ad84e14854
Anne Kreuter <anne.kreuter@outlook.de>
2021-01-20 15:06:39 +0100

    fixed two bugs: merge mit mehreren values + multiple compound fields mit x values werden x mal ausgegeben

--- v1/models/Translator.py
+++ v1/models/Translator.py
@@ -91,7 +91,18 @@ class MergeTranslator(Translator):
         for i in range(len(self.source_keys)):
             v = source_key_values.get(self.source_keys[i])
             list_of_values.append(v)
-        v_merged = self.merge_symbol.join(list_of_values)                              
+        if any(isinstance(i, list) for i in list_of_values):
+            if len(list_of_values) == 2:        # case: multiple values with 2 merge items
+                list1 = list_of_values[0]
+                list2 = list_of_values[1]
+                v_merged = [m + self.merge_symbol + n for m,n in zip(list1,list2)]
+            if len(list_of_values) == 3:        # case: multiple values with 3 merge items
+                list1 = list_of_values[0]
+                list2 = list_of_values[1]
+                list3 = list_of_values[2]
+                v_merged = [m + self.merge_symbol + n + self.merge_symbol + p for m,n,p in zip(list1,list2,list3)]
+        else:
+            v_merged = self.merge_symbol.join(list_of_values)                              
         return v_merged
     
     def get_priority(self):
