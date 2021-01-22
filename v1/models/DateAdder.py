ec7553f7755e47345644d809df8685b90452a686
Anne Kreuter <anne.kreuter@outlook.de>
2021-01-22 17:01:50 +0100

    DataAdder Klasse erstellt

---
+++ v1/models/DateAdder.py
@@ -0,0 +1,8 @@
+from datetime import date
+
+class DateAdder(object):
+    ''' returns Date of service '''
+    def main(self):
+        today = date.today()
+        date_formatted = today.strftime("%Y/%m/%d")
+        return date_formatted
