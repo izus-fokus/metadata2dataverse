2a23c568a19b91cb336be7276be76b395209d054
Florian Fritze <florian.fritze@ub.uni-stuttgart.de>
2025-08-25 09:24:27 +0200

    bugfix to let it run

--- v1/api/resources.py
+++ v1/api/resources.py
@@ -5,54 +5,48 @@ from models.Field import Field
 from models.TranslatorFactory import TranslatorFactory
 from builtins import isinstance
 from flask import abort, g
-
 TranslatorFactory = TranslatorFactory()
-from api.globals import MAPPINGS, DV_FIELD, DV_CHILDREN, DV_MB, CREDENTIALS_PATH  # global variables
+from api.globals import MAPPINGS, DV_FIELD, DV_CHILDREN, DV_MB, CREDENTIALS_PATH    #global variables
 import requests
 import json
 import logging
-
 logging.basicConfig(level=logging.INFO)
 
 
-def read_all_config_files():
+def read_all_config_files(): 
     """ Opens all config files located in './resources/config'  and gives them to read_config() method.
     
     If config file has no errors, it is transferred to fill_MAPPINGS() method. 
-    Otherwise abort with file errors.    
-    """
+    Otherwise, abort with file errors.
+    """    
     g.warnings = []
     rootdir = './resources/config'
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
             path = os.path.join(subdir, file)
-            open_yaml_file = open(path)
+            open_yaml_file = open(path)            
             config = read_config(open_yaml_file)
             open_yaml_file.close()
             # check if yaml file was correct    
             if len(g.warnings) > 0:
                 warnings = ' '.join(g.warnings)
-                abort(500, warnings)
+                abort(500, warnings)            
             fill_MAPPINGS(config)
-
+            
 
 # Read schema tsv files (metadatablocks nesting)      
 def read_all_scheme_files():
-    """ Opens all schemes from './resources/tsv' and gives them to read_scheme() method. """
-    with open(CREDENTIALS_PATH, "r") as cred_file:
+    """ Opens all schemes from './resources/tsv' and gives them to read_scheme() method. """    
+    with open(CREDENTIALS_PATH,"r") as cred_file:
         credentials = json.load(cred_file)
         dataverse_url = credentials["base_url"]
     try:
         read_scheme_from_api(dataverse_url + "api/metadatablocks/")
     except Exception as e:
-        print(
-            f"Error while loading metadata schemata: {e}")  # rootdir = './resources/tsv'        # for file in
-        # resources/resources  # for subdir, dirs, files in os.walk(rootdir):  #     for file in files:  #         path =
-        # os.path.join(subdir, file)  #         open_tsv_file = open(path, encoding="utf-8")  #         read_scheme(
-        # open_tsv_file)  #         open_tsv_file.close()
+        print (f"Error while loading metadata schemata: {e}")
 
 
-def read_config(data):
+def read_config(data):    
     """ Parses all information from config file, returns config object.
     
     Parameters
@@ -62,138 +56,61 @@ def read_config(data):
     Returns
     ------------
     config : Config obj    
-    """
+    """    
     g.warnings = []
-    yaml_file = yaml.safe_load(data)
+    yaml_file = yaml.safe_load(data)     
     # check for missing content
     content_list = ["scheme", "description", "formatSetting", "mapping"]
     for content in content_list:
         if content not in yaml_file:
-            g.warnings.append("{} missing in YAML file {}.".format(content, data))
+            g.warnings.append("{} missing in YAML file {}.".format(content, data))    
     if len(g.warnings) > 0:
-        return None  # Extracting dictionaries out of yaml-file
-    scheme = yaml_file["scheme"]
+        return None        
+    # Extracting dictionaries out of yaml-file   
+    scheme = yaml_file["scheme"]   
     description = yaml_file["description"]
     formatSetting = yaml_file["formatSetting"]
     mapping = yaml_file["mapping"]
     config = Config(scheme, description, formatSetting, yaml_file)
     # Create dict of translators out of the mapping    
     for translator_yaml in mapping:
-        config.add_translator(translator_yaml)  # Create dict of Rules
+        config.add_translator(translator_yaml)  
+    # Create dict of Rules
     if "rules" in yaml_file:
-        rules = yaml_file["rules"]
+        rules = yaml_file["rules"]      
         for rule_yaml in rules:
-            config.add_rules(rule_yaml)  # Create dict of namespaces
-    if "namespaces" in yaml_file:
-        namespaces = yaml_file["namespaces"]
-        if isinstance(namespaces, list):  # more than one namespace
+            config.add_rules(rule_yaml)                  
+    # Create dict of namespaces
+    if "namespaces" in yaml_file: 
+        namespaces = yaml_file["namespaces"]       
+        if isinstance(namespaces,list):     # more than one namespace
             for namespace in namespaces:
                 config.add_namespace(namespace)
         else:
-            config.add_namespace(namespaces)  # one namespace
-    return config
-
-
+            config.add_namespace(namespaces) # one namespace                       
+    return config    
+   
 def fill_MAPPINGS(config):
     """ Fills global MAPPINGS dictionary with config object.
     
-    Checks formatSetting of config file if scheme already exists. If formatSetting already exists: Abortion.
+    Checks format of config file if scheme already exists. If format already exists: Abortion.
     
     Parameters
     ---------
     config : obj      
     """
-
+    
     # fill global dictionary of mappings
     scheme = config.scheme
     if scheme in MAPPINGS:
         # check if mapping-formatSetting already exists
         for mapping in MAPPINGS[scheme]:
-            if mapping.format == config.format:
-                g.warnings.append(scheme + " with formatSetting " + mapping.format + " already existing")
+            if mapping.formatSetting == config.formatSetting:
+                g.warnings.append(scheme + " with formatSetting " + mapping.formatSetting + " already existing")
             else:
                 MAPPINGS[scheme].append(config)
-    else:
-        MAPPINGS[scheme] = [config]
-
-    # def read_scheme(data):
-
-
-""" This function has been replaced by read_scheme_from_api"""
-
-
-#     """ Parses all information from scheme, and saves information to the global dictionaries
-#         DV_FIELD, DV_CHILDREN, and DV_MB.         
-
-#     Parameters
-#     ---------
-#     data : opened tsv file    
-#     """    
-#     tsv_file = csv.reader(data, delimiter="\t")    
-#     start_metadata_block = False
-#     start_schema = False
-#     start_vocabulary = False    
-#     for row in tsv_file:      
-#         if not all('' == s or s.isspace() for s in row):
-#             row = [elem.strip() for elem in row]
-#             if(row[0] == "#datasetField"):            
-#                 try:
-#                     index_targetkey = row.index("name")
-#                     index_multiple = row.index("allowmultiples")
-#                     index_metadatablock = row.index("metadatablock_id")
-#                     index_fieldtype = row.index("fieldType")
-#                     index_parent = row.index("parent")
-#                     index_hascontrolledvoc = row.index("allowControlledVocabulary")
-#                 except ValueError as e:
-#                     print(row)
-#                     print("Check TSV #datasetField column names. Should contain name, allowmultiples, metadatablock_id,
-#                     fieldType, parent and allowControlledVocabulary.")
-#                     print(e)
-#                 start_schema = True
-#                 continue        
-#             if(row[0] == "#controlledVocabulary"):
-#                 try:
-#                     index_valuecontrolledvoc = row.index("Value")
-#                 except ValueError:
-#                         print("Check TSV #controlledVocabulary column names. Should contain Value.")
-#                 start_vocabulary = True
-#                 start_schema = False
-#                 continue        
-#             if(row[0] == "#metadataBlock"):
-#                 try:
-#                     index_mbname = row.index("name")
-#                     index_displayname = row.index("displayName")
-#                 except ValueError:
-#                     print("Check TSV #metadataBlock column names. Should contain displayName.")
-#                 start_metadata_block = True  
-#                 continue                                  
-#             if (start_metadata_block):
-#                 DV_MB[row[index_mbname]]=row[index_displayname]
-#                 start_metadata_block = False
-#                 continue                    
-#             if (start_schema):        
-#                 multiple = row[index_multiple]
-#                 parent = row[index_parent]
-#                 target_key = row[index_targetkey]            
-#                 if(parent == ""):
-#                     parent = None            
-#                 # check type (primitive, compound, controlled vocabulary)
-#                 type_class = "primitive"
-#                 metadata_block = row[index_metadatablock]
-#                 field_type = row[index_fieldtype]
-#                 if(row[index_fieldtype] == "none"):
-#                     type_class = "compound"      
-#                     DV_CHILDREN[target_key] = []      
-#                 if(row[index_hascontrolledvoc] == "TRUE"):
-#                     type_class = "controlled_vocabulary"            
-#                 # create parent/children map
-#                 if parent in DV_CHILDREN:
-#                     DV_CHILDREN[parent].append(target_key)                
-#                 field = Field(target_key, multiple, type_class, parent, metadata_block, field_type)             
-#                 DV_FIELD[target_key] = field                 
-#             if(start_vocabulary):
-#                 field = DV_FIELD[row[index_targetkey]]
-#                 field.set_controlled_vocabulary(row[index_valuecontrolledvoc])
+    else: 
+        MAPPINGS[scheme] = [config]    
 
 def get_field_object(field, block_name, parent=None):
     target_key = field['name']
@@ -212,6 +129,8 @@ def get_field_object(field, block_name, parent=None):
         field_obj.set_controlled_vocabulary(vocab_values)
 
     return field_obj
+
+
 
 
 def read_scheme_from_api(base_url):
@@ -241,11 +160,11 @@ def read_scheme_from_api(base_url):
         block_details = block_response.json().get('data', {})
         # logging.info("DV_MB2: {}".formatSetting(DV_MB2))
 
-        fields = block_details.get('fieldsElement', {})
+        fields = block_details.get('fields', {})
         for field_name, field in fields.items():
             field_obj = get_field_object(field, block_name)
 
-            # Handle child fieldsElement if the field is of type compound
+            # Handle child fields if the field is of type compound
             if field_obj.get_type_class() == "compound":
                 child_fields = field.get('childFields', {})
                 parent_name = field_obj.get_name()
@@ -261,6 +180,6 @@ def read_scheme_from_api(base_url):
                         DV_CHILDREN[parent_name] = []
                     if child_name not in DV_CHILDREN[parent_name]:
                         DV_CHILDREN[parent_name].append(child_name)
-            # child fieldsElement nicht doppelt
+            # child fields nicht doppelt
             if not field_obj.get_name() in DV_FIELD:
                 DV_FIELD[field_obj.get_name()] = field_obj
