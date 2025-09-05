13c2deab0b16cf6cc02c9374cbcb6143f58245b0
Florian Fritze <florian.fritze@ub.uni-stuttgart.de>
2025-09-05 13:22:29 +0200

    removed useless imports

--- v1/__init__.py
+++ v1/__init__.py
@@ -1,8 +1,5 @@
 import os
 from api.app import create_app
-from api import resources
-from api.resources import read_all_config_files, read_all_scheme_files
-from api.globals import MAPPINGS, DV_FIELD, DV_MB, DV_CHILDREN
 
 
 app = create_app()
